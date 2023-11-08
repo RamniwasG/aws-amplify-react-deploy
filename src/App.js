66b72619b56488acb5f7a88d9d332e504442b6fd
Ramniwas Gupta (TA Digital) <ramniwas.gupta@tadigital.com>
2023-11-08 16:18:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Welcome Raman</h3>
+        <p>
+          Your app has been deploy, published and hosted on AWS amplify
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
